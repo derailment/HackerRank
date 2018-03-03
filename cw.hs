baa307c4447b79124d4e36cc01d1b98ed0d14423
Abby <abby8050@gmail.com>
2018-03-03 17:19:38 +0000

    Crosswords part 1

--- cw.hs
+++ cw.hs
@@ -5,6 +5,24 @@ import Data.Function
 import Data.Foldable
 import Control.Applicative
 import Control.Monad
+
+testWords :: [String]
+testWords = splitOn ";" "LONDON;DELHI;ICELAND;ANKARA"
+
+testCrossWords :: CrossWords
+testCrossWords =
+  ["+-++++++++",
+   "+-++++++++",
+   "+-------++",
+   "+-++++++++",
+   "+-++++++++",
+   "+------+++",
+   "+-+++-++++",
+   "+++++-++++",
+   "+++++-++++",
+   "++++++++++"]
+
+{-
 main :: IO ()
 main = do
   hs <- getLines 10
@@ -12,12 +30,32 @@ main = do
   endline <- getLine
   let ws = splitOn ";" endline
   putStrLn $ show vs
+-}
+
+type CrossWords = [String]
+type Start = Int
+type End = Int
+type Id = Int
+data Slot = R Id (Start, End) | C Id (Start, End) deriving (Show)
+
+findSlots :: CrossWords -> [Slot]
+findSlots cw = concat [findSlot R i cw ++ findSlot C i (row2col cw) | i <- [0..9]]
+
+findSlot vh i rs = seg indices
+  where seg [] = []
+        seg is
+          | length rest /= 0 = if len > 1 then [vh i (head succ, last succ)] ++ seg rest else seg rest
+          | otherwise = if len > 1 then [vh i (head succ, last succ)] else []
+          where succ = pair is
+                rest = drop len is
+                len = length succ
+        pair (e:es) = foldl (\acc e -> if (last acc) + 1 == e then acc ++ [e] else acc) [e] es
+        indices = elemIndices '-' (rs !! i)
+
+row2col :: CrossWords -> CrossWords
+row2col rs = [[h !! j| h <- rs] | j <- [0..9]]
 
 
-zipHor :: [String] -> IO [String]
-zipHor hs = forM [0..length hs - 1] $ \i -> do
-  let v = [h !! i | h <- hs]
-  return v
 
 
 
