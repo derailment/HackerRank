80bce69364309e94e524fabdba5754afab32761f
Abby <abby8050@gmail.com>
2018-03-03 19:01:48 +0000

    Crossword part 2

--- cw.hs
+++ cw.hs
@@ -1,12 +1,10 @@
 import IO.Read
-import Data.List
+import Data.List as L
 import Data.List.Split
-import Data.Function
-import Data.Foldable
-import Control.Applicative
-import Control.Monad
+import Data.Foldable as F
+import Data.Map.Lazy as M
 
-testWords :: [String]
+testWords :: [Noun]
 testWords = splitOn ";" "LONDON;DELHI;ICELAND;ANKARA"
 
 testCrossWords :: CrossWords
@@ -22,6 +20,17 @@ testCrossWords =
    "+++++-++++",
    "++++++++++"]
 
+dic2list :: [Noun] -> [(Size, [(Noun, Slot)])]
+dic2list ns = M.toList $ buildDic ns $ fromList []
+
+buildDic :: [Noun] -> M.Map Size [(Noun, Slot)] -> M.Map Size [(Noun, Slot)]
+buildDic [] m = m
+buildDic (w:ws) m = buildDic ws map'
+  where map' = case M.lookup len m
+          of Nothing -> M.insert len [(w, EmptySlot)] m
+             Just _ -> M.insertWith (++) len [(w, EmptySlot)] m
+        len = length w
+
 {-
 main :: IO ()
 main = do
@@ -33,10 +42,12 @@ main = do
 -}
 
 type CrossWords = [String]
+type Noun = String
+type Size = Int
 type Start = Int
 type End = Int
 type Id = Int
-data Slot = R Id (Start, End) | C Id (Start, End) deriving (Show)
+data Slot = R Id (Start, End) | C Id (Start, End) | EmptySlot deriving (Show)
 
 findSlots :: CrossWords -> [Slot]
 findSlots cw = concat [findSlot R i cw ++ findSlot C i (row2col cw) | i <- [0..9]]
@@ -49,11 +60,12 @@ findSlot vh i rs = seg indices
           where succ = pair is
                 rest = drop len is
                 len = length succ
-        pair (e:es) = foldl (\acc e -> if (last acc) + 1 == e then acc ++ [e] else acc) [e] es
+        pair (e:es) = F.foldl (\acc e -> if (last acc) + 1 == e then acc ++ [e] else acc) [e] es
         indices = elemIndices '-' (rs !! i)
 
 row2col :: CrossWords -> CrossWords
 row2col rs = [[h !! j| h <- rs] | j <- [0..9]]
+
 
 
 
