19c1803e8e6e1facbfb560b2a30c4b72f21f397a
Abby <abby8050@gmail.com>
2018-03-04 13:26:19 +0000

    Crossword part 4

--- cw.hs
+++ cw.hs
@@ -5,42 +5,32 @@ import Data.Foldable as F
 import Data.Map.Lazy as M
 
 testWords :: [Noun]
-testWords = splitOn ";" "LONDON;DELHI;ICELAND;ANKARA"
+testWords = splitOn ";" "ANKARA;LONDON;DELHI;ICELAND"
 
 testCrossWords :: CrossWords
 testCrossWords =
   ["+-++++++++",
    "+-++++++++",
-   "+-------++",
    "+-++++++++",
-   "+-++++++++",
-   "+------+++",
+   "+-----++++",
+   "+-+++-++++",
+   "+-+++-++++",
    "+++++-++++",
-   "+++++-++++",
+   "++------++",
    "+++++-++++",
    "+++++-++++"]
 
-buildDic :: [Noun] -> M.Map Size [(Noun, Slot)] -> M.Map Size [(Noun, Slot)]
-buildDic [] m = m
-buildDic (w : ws) m = buildDic ws m'
-  where m' = case M.lookup len m
-                 of Nothing -> M.insert len [(w, EmptySlot)] m
-                    Just _ -> M.insertWith (++) len [(w, EmptySlot)] m
-        len = length w
+testDic = pasteSlots (findSlots testCrossWords) (buildDic testWords (fromList []))
 
-pasteSlots :: [Slot] -> M.Map Size [(Noun, Slot)] -> M.Map Size [(Noun, Slot)]
-pasteSlots [] m = m
-pasteSlots (l : ls) m = pasteSlots ls (M.insert size (p (getPairs size m) l) m)
-  where getPairs size m = case M.lookup size m
-                            of Nothing -> []
-                               Just ns -> ns
-        getSE (R i (s, e)) = (s, e)
-        getSE (C i (s, e)) = (s, e)
-        p ((n, s) : ns) slot = if s == EmptySlot then (n, slot) : ns else (n, s) : p ns slot
-        size = let (s, e) = getSE l in e - s + 1
+testValid = validate testCrossWords testDic
 
-test = pasteSlots (findSlots testCrossWords) (buildDic testWords (fromList []))
-
+type CrossWords = [String]
+type Noun = String
+type Size = Int
+type Start = Int
+type End = Int
+type Id = Int
+data Slot = R Id (Start, End) | C Id (Start, End) | EmptySlot deriving (Show, Eq)
 
 validate :: CrossWords -> M.Map Size [(Noun, Slot)] -> Maybe CrossWords
 validate cw m = f (Just cw) (M.toList m)
@@ -50,10 +40,11 @@ validate cw m = f (Just cw) (M.toList m)
           where f' Nothing _ = Nothing
                 f' (Just cw) [] = Just cw
                 f' (Just cw) ((n, R i (s, e)) : ns) = f' (fill cw i (s, e) n) ns
-                f' (Just cw) ((n, C i (s, e)) : ns) = f' --transpose again-- (fill (row2col cw) i (s, e) n) ns
+                f' (Just cw) ((n, C i (s, e)) : ns) = case tcw
+                  of Nothing -> Nothing
+                     Just tcw' -> f' (Just (trans tcw')) ns
+                  where tcw = (fill (trans cw) i (s, e) n)
 
-testDic :: M.Map Size [(Noun, Slot)]
-testDic = fromList [(6, [("LONDON", C 1 (0, 5)), ("ANKARA", R 5 (1, 6))]), (5, [("DELHI", C 5 (5, 9))]), (7, [("ICELAND", R 2 (1, 7))])]
 
 fill cw i (s, e) n = case any (\(a, b) -> a /= '-' && a /= b) [(a, b) | (a, b) <- zip l n]
                        of True -> Nothing
@@ -66,40 +57,19 @@ fill cw i (s, e) n = case any (\(a, b) -> a /= '-' && a /= b) [(a, b) | (a, b) <
                                        n' = fs' ++ n ++ bs';
                                     in fs ++ [n'] ++ bs
 
-testCW =
-  ["+------+++",
-   "+-++++++++",
-   "+---E---++",
-   "+-++++++++",
-   "+-++++++++",
-   "+------+++",
-   "+++++-++++",
-   "+++++-++++",
-   "+++++-++++",
-   "+++----+++"]
-
-testFill = fill testCW 2 (1, 7) "ABCEDEFG"
-
-{-
-main :: IO ()
-main = do
-  hs <- getLines 10
-  vs <- zipHor hs
-  endline <- getLine
-  let ws = splitOn ";" endline
-  putStrLn $ show vs
--}
-
-type CrossWords = [String]
-type Noun = String
-type Size = Int
-type Start = Int
-type End = Int
-type Id = Int
-data Slot = R Id (Start, End) | C Id (Start, End) | EmptySlot deriving (Show, Eq)
+pasteSlots :: [Slot] -> M.Map Size [(Noun, Slot)] -> M.Map Size [(Noun, Slot)]
+pasteSlots [] m = m
+pasteSlots (l : ls) m = pasteSlots ls (M.insert size (p (getPairs size m) l) m)
+  where getPairs size m = case M.lookup size m
+                            of Nothing -> []
+                               Just ns -> ns
+        getSE (R i (s, e)) = (s, e)
+        getSE (C i (s, e)) = (s, e)
+        p ((n, s) : ns) slot = if s == EmptySlot then (n, slot) : ns else (n, s) : p ns slot
+        size = let (s, e) = getSE l in e - s + 1
 
 findSlots :: CrossWords -> [Slot]
-findSlots cw = concat [findSlot R i cw ++ findSlot C i (row2col cw) | i <- [0..9]]
+findSlots cw = concat [findSlot R i cw ++ findSlot C i (trans cw) | i <- [0..9]]
 
 findSlot vh i rs = seg indices
   where seg [] = []
@@ -112,8 +82,28 @@ findSlot vh i rs = seg indices
         pair (e:es) = F.foldl (\acc e -> if (last acc) + 1 == e then acc ++ [e] else acc) [e] es
         indices = elemIndices '-' (rs !! i)
 
-row2col :: CrossWords -> CrossWords
-row2col rs = [[h !! j| h <- rs] | j <- [0..9]]
+trans :: CrossWords -> CrossWords
+trans rs = [[h !! j| h <- rs] | j <- [0..9]]
+
+buildDic :: [Noun] -> M.Map Size [(Noun, Slot)] -> M.Map Size [(Noun, Slot)]
+buildDic [] m = m
+buildDic (w : ws) m = buildDic ws m'
+  where m' = case M.lookup len m
+                 of Nothing -> M.insert len [(w, EmptySlot)] m
+                    Just _ -> M.insertWith (++) len [(w, EmptySlot)] m
+        len = length w
+
+{-
+main :: IO ()
+main = do
+  hs <- getLines 10
+  vs <- zipHor hs
+  endline <- getLine
+  let ws = splitOn ";" endline
+  putStrLn $ show vs
+-}
+
+
 
 
 
